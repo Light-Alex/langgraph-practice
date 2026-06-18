e360777e50f70595c099856b509801f41206191d
Light-Alex <245212467@qq.com>
2026-06-18 15:47:42 +0800

    add evaluator-optimizer agent to localserver-app

--- localserver-app/src/agent/__init__.py
+++ localserver-app/src/agent/__init__.py
@@ -6,5 +6,6 @@ This module defines a custom graph.
 from agent.email_agent import app
 from agent.graph import graph
 from agent.orchestrator_worker import orchestrator_worker
+from agent.evaluator_optimizer import optimizer_workflow
 
-__all__ = ["graph", "app", "orchestrator_worker"]
+__all__ = ["graph", "app", "orchestrator_worker", "optimizer_workflow"]
